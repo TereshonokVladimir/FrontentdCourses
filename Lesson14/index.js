ab478a85feb38c34132091bf45c330ac9994cdde
Vladimir <tereshonokvova@gmail.com>
2024-05-01 17:20:21 +0300

    Lesson14js

---
+++ Lesson14/index.js
@@ -0,0 +1,40 @@
+
+
+// let num = 42
+// let str = ''
+// let bool = false
+
+
+// let num1 = num+''
+// let num2 = toString(num)
+
+// let num3 = !!num
+// let num4 = Boolean(num)
+
+// // console.log(num3,num4)
+
+// let str1 = +str
+// let str2 = Number(str)
+
+// let str3 = !!str
+// let str4 = Boolean(str)
+
+// // console.log(str1,str4)
+
+
+// let bool1 = +bool
+// let bool2 = Number(bool)
+
+// let bool3 = bool+""
+// let bool4 = toString(bool)
+
+// console.log(bool1,bool3)
+
+
+let num = 45//number
+let str = 'string'//string
+let bool = true
+// - , / , * number
+// + string
+// console.log(num)
+
